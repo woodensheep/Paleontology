da60108625df4c2b6cee4941542bee9ac10fa8f4
qingsong <1154046205@qq.com>
2017-05-18 10:02:01 +0800

    登录设置页面

---
+++ app/src/main/java/com/nandity/paleontology/util/ScreenZoomUtil.java
@@ -0,0 +1,50 @@
+package com.nandity.paleontology.util;
+
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.view.View;
+
+/**
+ * Created by qingsong on 2017/5/17.
+ */
+
+public class ScreenZoomUtil {
+   public static  float scale = 0.6f; //logo缩放比例
+    /**
+     * 缩小
+     * @param view
+     */
+    public static void zoomIn(final View view, float dist) {
+        view.setPivotY(view.getHeight());
+        view.setPivotX(view.getWidth() / 2);
+        AnimatorSet mAnimatorSet = new AnimatorSet();
+        ObjectAnimator mAnimatorScaleX = ObjectAnimator.ofFloat(view, "scaleX", 1.0f, scale);
+        ObjectAnimator mAnimatorScaleY = ObjectAnimator.ofFloat(view, "scaleY", 1.0f, scale);
+        ObjectAnimator mAnimatorTranslateY = ObjectAnimator.ofFloat(view, "translationY", 0.0f, -dist);
+
+        mAnimatorSet.play(mAnimatorTranslateY).with(mAnimatorScaleX);
+        mAnimatorSet.play(mAnimatorScaleX).with(mAnimatorScaleY);
+        mAnimatorSet.setDuration(200);
+        mAnimatorSet.start();
+    }
+
+    /**
+     * f放大
+     * @param view
+     */
+    public static void zoomOut(final View view, float dist) {
+        view.setPivotY(view.getHeight());
+        view.setPivotX(view.getWidth() / 2);
+        AnimatorSet mAnimatorSet = new AnimatorSet();
+
+        ObjectAnimator mAnimatorScaleX = ObjectAnimator.ofFloat(view, "scaleX", scale, 1.0f);
+        ObjectAnimator mAnimatorScaleY = ObjectAnimator.ofFloat(view, "scaleY", scale, 1.0f);
+        ObjectAnimator mAnimatorTranslateY = ObjectAnimator.ofFloat(view, "translationY", view.getTranslationY(), 0);
+
+        mAnimatorSet.play(mAnimatorTranslateY).with(mAnimatorScaleX);
+        mAnimatorSet.play(mAnimatorScaleX).with(mAnimatorScaleY);
+        mAnimatorSet.setDuration(200);
+        mAnimatorSet.start();
+    }
+
+}
