aced0cf0caf7ea6db7159410b4f3d1e275e4292d
qingsong <1154046205@qq.com>
2017-05-19 11:29:31 +0800

    fossil

--- app/src/main/java/com/nandity/paleontology/FossilDate/FossilDateActivity.java
+++ app/src/main/java/com/nandity/paleontology/FossilDate/FossilDateActivity.java
@@ -1,24 +1,21 @@
-package com.nandity.paleontology.relicdata.ui;
+package com.nandity.paleontology.FossilDate;
 
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Spinner;
+import android.widget.RelativeLayout;
 
 import com.lzy.okgo.OkGo;
 import com.lzy.okgo.callback.StringCallback;
 import com.nandity.paleontology.R;
 import com.nandity.paleontology.personneldata.PersonnelApi;
+import com.nandity.paleontology.relicdata.ui.PaleontoAdapter;
+import com.nandity.paleontology.relicdata.ui.PaleontologicalActivity;
 import com.nandity.paleontology.relicdata.util.PaleGsonHelper;
 import com.nandity.paleontology.relicdata.util.PaleontologicalaBean;
-import com.nandity.paleontology.util.ToActivityUtlis;
 
 import java.util.List;
 
@@ -27,75 +24,52 @@ import butterknife.ButterKnife;
 import okhttp3.Call;
 import okhttp3.Response;
 
-public class PaleontologicalActivity extends AppCompatActivity {
+import static com.nandity.paleontology.R.id.etSearch;
 
-    @BindView(R.id.spinner1)
-    Spinner spinner1;
-    @BindView(R.id.etSearch)
-    EditText etSearch;
-    @BindView(R.id.btnSearch)
-    Button btnSearch;
+public class FossilDateActivity extends AppCompatActivity {
+
+    @BindView(R.id.rl_title)
+    RelativeLayout rlTitle;
     @BindView(R.id.date_show)
     RecyclerView dateShow;
     @BindView(R.id.date_refresh)
     SwipeRefreshLayout dateRefresh;
     private LinearLayoutManager mLinearLayoutManger;
-    private String spinnerType;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_paleontological);
+        setContentView(R.layout.activity_fossil_date);
         ButterKnife.bind(this);
         initListener();
     }
 
     private void initListener() {
-        //下拉框选择监听
-        spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view,
-                                       int pos, long id) {
 
-                String[] languages = getResources().getStringArray(R.array.personnel_date);
-                spinnerType=languages[pos];
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-                // Another interface callback
-            }
-        });
         //下拉刷新
         dateRefresh.setColorSchemeResources(R.color.colorPrimary);
         dateRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
-                initView(spinnerType,etSearch.getText().toString());
+                initView();
                 dateRefresh.setRefreshing(false);
             }
         });
-        //搜索
-        btnSearch.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //initView(spinnerType,etSearch.getText().toString());
-                ToActivityUtlis.toNextActivity(PaleontologicalActivity.this,ReLicDataActivity.class);
-            }
-        });
+
     }
 
 
-    private void initView(String spinnerType, String searchText) {
+    private void initView() {
         OkGo.post(PersonnelApi.DATE_Personnel)
-                .params("type", spinnerType)
-                .params("searchText", searchText)
+                .params("type", "")
+                .params("searchText", "")
                 .execute(new StringCallback() {
                     @Override
                     public void onSuccess(String s, Call call, Response response) {
                         List<PaleontologicalaBean> mPaleontoBeanList = PaleGsonHelper.mPaleontoBeanList(s);
                         dateShow.setLayoutManager(mLinearLayoutManger);
-                        dateShow.setAdapter(new PaleontoAdapter(PaleontologicalActivity.this,mPaleontoBeanList));
+                        dateShow.setAdapter(new FossiAdapter(FossilDateActivity.this, mPaleontoBeanList));
                     }
                 });
     }
@@ -103,5 +77,6 @@ public class PaleontologicalActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
+
     }
 }
--- app/src/main/java/com/nandity/paleontology/relicdata/ui/ReLicDataActivity.java
+++ app/src/main/java/com/nandity/paleontology/relicdata/ui/ReLicDataActivity.java
@@ -2,8 +2,26 @@ package com.nandity.paleontology.relicdata.ui;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.RelativeLayout;
 
+import com.lzy.okgo.OkGo;
+import com.lzy.okgo.callback.StringCallback;
+import com.nandity.paleontology.FossilDate.FossiAdapter;
+import com.nandity.paleontology.FossilDate.FossilDateActivity;
 import com.nandity.paleontology.R;
+import com.nandity.paleontology.personneldata.PersonnelApi;
+import com.nandity.paleontology.relicdata.util.PaleGsonHelper;
+import com.nandity.paleontology.relicdata.util.PaleontologicalaBean;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import okhttp3.Call;
+import okhttp3.Response;
 
 /**
  * Created by lemon on 2017/5/18.
@@ -11,15 +29,55 @@ import com.nandity.paleontology.R;
 
 public class ReLicDataActivity extends FragmentActivity {
 
-
-
+    @BindView(R.id.rl_title)
+    RelativeLayout rlTitle;
+    @BindView(R.id.date_show)
+    RecyclerView dateShow;
+    @BindView(R.id.date_refresh)
+    SwipeRefreshLayout dateRefresh;
+    private LinearLayoutManager mLinearLayoutManger;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_relic_data);
 
+        ButterKnife.bind(this);
+        initListener();
+    }
+
+    private void initListener() {
+
+        //下拉刷新
+        dateRefresh.setColorSchemeResources(R.color.colorPrimary);
+        dateRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                initView();
+                dateRefresh.setRefreshing(false);
+            }
+        });
+
     }
 
 
+    private void initView() {
+        OkGo.post(PersonnelApi.DATE_Personnel)
+                .params("type", "")
+                .params("searchText", "")
+                .execute(new StringCallback() {
+                    @Override
+                    public void onSuccess(String s, Call call, Response response) {
+                        List<PaleontologicalaBean> mPaleontoBeanList = PaleGsonHelper.mPaleontoBeanList(s);
+                        dateShow.setLayoutManager(mLinearLayoutManger);
+                        dateShow.setAdapter(new FossiAdapter(ReLicDataActivity.this, mPaleontoBeanList));
+                    }
+                });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+    }
 }
